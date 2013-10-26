c35890280d1af330dd98e7336dbe50fdcbac72b5
Graham Walters <Graham@Grahams-MacBook.local>
2013-10-26 04:54:48 +0100

    Changed window size to 800x480 & renamed window

--- TheDrunkenLeprechaun-desktop/src/com/NapierDevSoc/TheDrunkenLeprechaun/Main.java
+++ TheDrunkenLeprechaun-desktop/src/com/NapierDevSoc/TheDrunkenLeprechaun/Main.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "TheDrunkenLeprechaun";
+		cfg.title = "The Drunken Leprechaun";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 800;
+		cfg.height = 480;
 		
 		new LwjglApplication(new DrunkenLeprechaun(), cfg);
 	}
