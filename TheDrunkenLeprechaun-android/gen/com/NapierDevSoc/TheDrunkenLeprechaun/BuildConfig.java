d20676672453cea529710d2768fd362aef17c84e
Sean Jones <neuralsandwich@gmail.com>
2013-10-26 02:51:33 +0100

    Changed image to a leprechaun

---
+++ TheDrunkenLeprechaun-android/gen/com/NapierDevSoc/TheDrunkenLeprechaun/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.NapierDevSoc.TheDrunkenLeprechaun;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
